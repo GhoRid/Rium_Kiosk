362b46c31c689699de9dbfe24f0d580eca9824f9
unknown <pgh5489@gmail.com>
2025-08-03 23:06:17 +0900

    feat: 신용 승인 코드 수정 및 작동 확인

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,9 +1,15 @@
-import axios, { AxiosInstance } from "axios";
+import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http:localhost:9188", // 실제 배포된 백엔드 URL로 변경
+  baseURL: "http://127.0.0.1:9188", // 백엔드 URL
   timeout: 30000,
   headers: {
-    "Content-Type": "text/plain",
+    "Content-Type": "application/x-www-form-urlencoded", // Form Data로 전송
   },
+  transformRequest: [
+    (data) => {
+      // data를 그대로 문자열로 변환
+      return typeof data === "string" ? data : String(data);
+    },
+  ],
 });
--- src/apis/paymentApi.ts
+++ src/apis/paymentApi.ts
@@ -2,32 +2,25 @@ import { instance } from ".";
 import { createPaymentBuffer } from "../utils/paymentUtils";
 import { makeSendData } from "../utils/vcatUtils";
 
-/**
- * 결제 요청/중지 요청 전송 (VCAT 패킷 자동 생성 포함)
- * @param type 결제 타입 ("credit" | "credit_fallback" | "credit_cancel" | "stop")
- * @param form 결제 데이터 (sendMsg 대신 form 직접 전달)
- */
 export const sendPayment = async (sendData: string) => {
   let sendMsg: string;
 
-  // 1️⃣ 결제 데이터 생성
-
-  // 2️⃣ VCAT 패킷 생성
+  // 1. VCAT 패킷 생성
   sendMsg = makeSendData(sendData);
 
   console.log("sendData : ", sendData, "sendMsg : ", sendMsg);
 
-  // 3️⃣ URI 인코딩
+  // 2. URI 인코딩
   const sendbuf = encodeURI(sendMsg);
 
   console.log("sendbuf : ", sendbuf);
 
-  // 4️⃣ 전송 (포트 분기 처리)
+  // 3. 전송 (포트 분기 처리)
   // const url =
   //   type === "stop" ? "http://127.0.0.1:9189" : "http://127.0.0.1:9188";
 
   try {
-    const { data } = await instance.post(sendbuf);
+    const { data } = await instance.post("/", sendbuf);
     return data;
   } catch (error) {
     console.error("결제 요청 실패:", error);
